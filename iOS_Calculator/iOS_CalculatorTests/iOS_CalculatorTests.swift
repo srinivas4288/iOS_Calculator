ef3f8c8dcf3187e2dcb8a3811246fa2695d1cf0b
Srinivas Muppera <srinivasaraomuppera4288@gmail.com>
2025-02-05 14:16:59 +0530

    Add files via upload

---
+++ iOS_Calculator/iOS_CalculatorTests/iOS_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CalculatorTests.swift
+//  iOS_CalculatorTests
+//
+//  Created by Sudheer Arava on 04/02/25.
+//
+
+import Testing
+@testable import iOS_Calculator
+
+struct iOS_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
